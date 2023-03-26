c9133191c9869fcdf80d0fbaa53cd1df0ce257e9
Franciszek Łomiński <mr.francesco.lomi@gmail.com>
2023-03-26 22:20:52 +0200

    Add project files.

---
+++ LANChatApp.cpp
@@ -0,0 +1,18 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
